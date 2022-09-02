4ec8eeb9b35eda1046f0b59c480a4c165b072ac0
ButchBet <butchbet01@gmail.com>
2022-09-02 13:55:19 -0500

    Added style to the panel of settings and questions, also created script to close and open the settings panel.

--- js/script.js
+++ js/script.js
@@ -1,47 +1,63 @@
-const options = ['rock', 'paper', 'scissors']; 
+// const options = ['rock', 'paper', 'scissors']; 
 
-        function checkWiner(user = null, cpu = null) {
-            if((user === options[0] && cpu === options[2]) || (user === options[1] && cpu === options[0]) || (user === options[2] && cpu === options[1])) {
-                alert("You win!.");
-            } else if((user === options[2] && cpu === options[0]) || (user === options[0] && cpu === options[1]) || (user === options[1] && cpu === options[2])) {
-                alert("The cpu wins!.")
-            } else {
-                alert("Tie!");
-            }
-        }
+// function checkWiner(user = null, cpu = null) {
+//     if((user === options[0] && cpu === options[2]) || (user === options[1] && cpu === options[0]) || (user === options[2] && cpu === options[1])) {
+//         alert("You win!.");
+//     } else if((user === options[2] && cpu === options[0]) || (user === options[0] && cpu === options[1]) || (user === options[1] && cpu === options[2])) {
+//         alert("The cpu wins!.")
+//     } else {
+//         alert("Tie!");
+//     }
+// }
 
-        function setOption() {
-            let stop = false; // Stop the main loop
+// function setOption() {
+//     let stop = false; // Stop the main loop
 
-            do {
-                let user;
-                // Get the index in order to get the option of the cpu
-                let index = Math.floor(Math.random() * 3);
-                // Set the option of the cpu
-                let cpu = options[index];
-    
-                // Set the option of the user
-                user = Number(prompt("Hello!, please choose your option: 0. rock, 1. paper and 2. scissors"));
+//     do {
+//         let user;
+//         // Get the index in order to get the option of the cpu
+//         let index = Math.floor(Math.random() * 3);
+//         // Set the option of the cpu
+//         let cpu = options[index];
 
-                // Check that the input is correct
-                if(user === 0 || user === 1 || user === 2) {
-                    user = options[user];
-                    
-                    checkWiner(user, cpu);
-                } else {
-                    alert("Please enter a valid option")
-                }
+//         // Set the option of the user
+//         user = Number(prompt("Hello!, please choose your option: 0. rock, 1. paper and 2. scissors"));
 
-                let option = Number(prompt("Would you like to continue?: 0. no and any other number. yes"));
+//         // Check that the input is correct
+//         if(user === 0 || user === 1 || user === 2) {
+//             user = options[user];
+            
+//             checkWiner(user, cpu);
+//         } else {
+//             alert("Please enter a valid option")
+//         }
 
-                // change the value of stop
-                if(option) {
-                    stop = false;
-                } else {
-                    stop = true;
-                }
-            } while(!stop)
+//         let option = Number(prompt("Would you like to continue?: 0. no and any other number. yes"));
 
-        }
+//         // change the value of stop
+//         if(option) {
+//             stop = false;
+//         } else {
+//             stop = true;
+//         }
+//     } while(!stop)
 
-        setOption()
+// }
+
+// setOption()
+
+// Script to close and open the settings panel
+const openSettingAndQuestions = document.getElementById('openSettingAndQuestions');
+const settingsPanel = document.getElementById('settingsPanel');
+const closeSettingAndQuestions = document.getElementById('closeSettingAndQuestions');
+
+openSettingAndQuestions.addEventListener('click', (e) => {
+    openSettingAndQuestions.classList.add('hidden'); // Hide the setthing and questions icon
+    settingsPanel.classList.remove('hidden'); // Show up the settings panel
+});
+
+
+closeSettingAndQuestions.addEventListener('click', (e) => {
+    settingsPanel.classList.add('hidden'); // Hide the panel
+    openSettingAndQuestions.classList.remove('hidden'); // Show up the setthing and questions icon
+});
